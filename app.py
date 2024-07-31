b98d0e7236fde75b2edfaaadc97a69ddc028835e
g451 <g451@gmail.com>
2024-07-31 10:31:51 -0300

    b

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/simulacion/<int:param1>/<int:param2>', methods=['GET', 'POST'])
-def simulacion(param1, param2):
+
+@app.route('/simulacion/<int:cantidadLineasASimular>/<int:duracionSimulacion>/<int:lineaInicioVisualizacion>'
+        '/<int:lineaFinVisualizacion>/<int:cantidadSurtidores>/<int:cantidadEmpleadosGomeria>/<int:cantidadEmpleadosVentaAccesorios>'
+        '/<float:llegadaClientesMedia>/<float:llegadaClientesDesviacion>/<float:aDuracionCargaCombustible>/<float:bDuracionCargaCombustible>'
+        '/<float:aDuracionAtGomeria>/<float:bDuracionAtGomeria>/<float:aDuracionVentaAccesorios>/<float:bDuracionVentaAccesorios>', methods=['GET', 'POST'])
+def simulacion(cantidadLineasASimular, duracionSimulacion, lineaInicioVisualizacion,
+            lineaFinVisualizacion, cantidadSurtidores, cantidadEmpleadosGomeria, cantidadEmpleadosVentaAccesorios,
+            llegadaClientesMedia, llegadaClientesDesviacion, aDuracionCargaCombustible, bDuracionCargaCombustible,
+            aDuracionAtGomeria, bDuracionAtGomeria, aDuracionVentaAccesorios, bDuracionVentaAccesorios):
     # Aquí puedes agregar lógica para manejar los parámetros
-    param1 += 1
-    param2 += 10000000
     data = {
-        "duracionSimulacion": param1,
-        "lineaInicioSimulacion": param2
+        "cantidadLineasASimular": cantidadLineasASimular,
+        "duracionSimulacion": duracionSimulacion,
+        "lineaInicioVisualizacion": lineaInicioVisualizacion,
+        "lineaFinVisualizacion": lineaFinVisualizacion,
+        "cantidadSurtidores": cantidadSurtidores,
+        "cantidadEmpleadosGomeria": cantidadEmpleadosGomeria,
+        "cantidadEmpleadosVentaAccesorios": cantidadEmpleadosVentaAccesorios,
+        "llegadaClientesMedia": llegadaClientesMedia,
+        "llegadaClientesDesviacion": llegadaClientesDesviacion,
+        "aDuracionCargaCombustible": aDuracionCargaCombustible,
+        "bDuracionCargaCombustible": bDuracionCargaCombustible,
+        "aDuracionAtGomeria": aDuracionAtGomeria,
+        "bDuracionAtGomeria": bDuracionAtGomeria,
+        "aDuracionVentaAccesorios": aDuracionVentaAccesorios,
+        "bDuracionVentaAccesorios": bDuracionVentaAccesorios,
     }
+
+    # Aquí puedes agregar la lógica de simulación utilizando los parámetros
+    # Esta es una muestra de modificación de parámetros para mostrar que funciona
+    data["duracionSimulacion"] += 1
+    data["lineaInicioVisualizacion"] += 10000000
+
     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
+
